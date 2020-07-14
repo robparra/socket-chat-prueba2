7584c1e675457c0762aca79f1529a91ed1faf7f4
robert <parralrob@gmail.com>
2020-07-14 15:04:23 -0400

    prueba chat privado

---
+++ public/js/socket-chat-jquery.js
@@ -0,0 +1,181 @@
+var params = new URLSearchParams(window.location.search);
+
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
+
+// referencias de jQuery
+var divUsuarios = $('#divUsuarios');
+var divUsuarios2 = $('#divUsuarios2');
+
+var formEnviar = $('#formEnviar');
+var formEnviar2 = $('#formEnviar2');
+
+var txtMensaje = $('#txtMensaje');
+var txtMensaje2 = $('#txtMensaje2');
+
+var divChatbox = $('#divChatbox');
+var divChatbox2 = $('#divChatbox2');
+
+
+// Funciones para renderizar usuarios
+function renderizarUsuarios(personas) { // [{},{},{}]
+
+    console.log(personas);
+
+    var html = '';
+
+    html += '<li>';
+    html += '    <a href="javascript:void(0)" class="active"> Room: <span> ' + params.get('sala') + '</span></a>';
+    html += '</li>';
+
+    for (var i = 0; i < personas.length; i++) {
+
+        html += '<li>';
+        html += '    <a data-id="' + personas[i].id + '"  href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>' + personas[i].nombre + ' <small class="text-success">online</small></span></a>';
+        html += '</li>';
+    }
+
+    divUsuarios.html(html);
+
+}
+
+
+function renderizarMensajes(mensaje, yo) {
+
+    var html = '';
+    var fecha = new Date(mensaje.fecha);
+    var hora = fecha.getHours() + ':' + fecha.getMinutes();
+
+    var adminClass = 'info';
+    if (mensaje.nombre === 'Administrador') {
+        adminClass = 'danger';
+    }
+
+    if (yo) {
+        html += '<li class="reverse">';
+        html += '    <div class="chat-content">';
+        html += '        <h5>' + mensaje.nombre + '</h5>';
+        html += '        <div class="box bg-light-inverse">' + mensaje.mensaje + '</div>';
+        html += '    </div>';
+        html += '    <div class="chat-img"><img src="assets/images/users/5.jpg" alt="user" /></div>';
+        html += '    <div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+
+    } else {
+
+        html += '<li class="animated fadeIn">';
+
+        if (mensaje.nombre !== 'Administrador') {
+            html += '    <div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>';
+        }
+
+        html += '    <div class="chat-content">';
+        html += '        <h5>' + mensaje.nombre + '</h5>';
+        html += '        <div class="box bg-light-' + adminClass + '">' + mensaje.mensaje + '</div>';
+        html += '    </div>';
+        html += '    <div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+
+    }
+
+
+    divChatbox.append(html);
+
+}
+
+function scrollBottom() {
+
+    // selectors
+    var newMessage = divChatbox.children('li:last-child');
+
+    // heights
+    var clientHeight = divChatbox.prop('clientHeight');
+    var scrollTop = divChatbox.prop('scrollTop');
+    var scrollHeight = divChatbox.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
+
+
+function scrollBottom2() {
+
+    // selectors
+    var newMessage = divChatbox2.children('li:last-child');
+
+    // heights
+    var clientHeight = divChatbox2.prop('clientHeight');
+    var scrollTop = divChatbox2.prop('scrollTop');
+    var scrollHeight = divChatbox2.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
+
+
+// Listeners
+divUsuarios.on('click', 'a', function() {
+
+    //limpiarMensaje();
+ 
+ // LLamamos a la etiqueta dentro de ancortag (a) que contiene el id
+ var id = $(this).data('id');
+ 
+ if (id) {
+ console.log(id);
+ //window.open("privado.html","privado","with=120, height=300,scrollbars=YES")
+ abrirModal();
+ //limpiarMensaje();
+ 
+// formulario del que envía al tocar el usuario de la izquierda
+ formEnviar2.on('submit', function(e) {
+ 
+ e.preventDefault();
+ 
+ // verificar si el mensaje viene vacío
+ // trim quita los espacios al principio y al final
+ if (txtMensaje2.val().trim().length === 0) {
+ return;
+ }
+ 
+ socket.emit('mensajePrivado', {
+ nombre: nombre,
+ mensaje: txtMensaje2.val(),
+ para: id
+ }, function(mensaje) {
+ txtMensaje2.val('').focus();
+ renderizarMensajePrivado(mensaje, true);
+ scrollBottom2();
+ return;
+ });
+ });
+ }
+ return;
+});
+
+formEnviar.on('submit', function(e) {
+
+    e.preventDefault();
+
+    if (txtMensaje.val().trim().length === 0) {
+        return;
+    }
+
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje, true);
+        scrollBottom();
+    });
+
+
+});
---
+++ server/sockets/socket.js
@@ -0,0 +1,70 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'User/Room is necessary'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } join the chat`));
+
+        callback(usuarios.getPersonasPorSala(data.sala));
+
+    });
+
+    client.on('crearMensaje', (data, callback) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
+    });
+
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } left the chat`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+
+
+    });
+
+    // Mensajes privados
+    client.on('mensajePrivado', (data, callback) => {
+ // obtener a la persona que envía el mensaje
+ let persona = usuarios.getPersona(client.id);
+ // crear mensaje
+ let mensaje = crearMensaje(persona.nombre, data.mensaje, client.id);
+ // console.log(mensaje);
+ client.broadcast.to(data.para).emit('mensajePrivado', mensaje);
+ callback(mensaje);
+ });
+
+    //Socket image
+  /* This function cacth the image and emit it to all users in the room
+  */
+  client.on('user image',function(image){
+      let persona = usuarios.getPersona(client.id);
+      client.broadcast.to(data.sala).emit('addimage', `Image shared by ${data.nombre}: `, image);
+  });
+
+});
